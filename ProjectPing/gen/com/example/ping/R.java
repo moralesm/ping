46b2d2aec47caa156d9d7c6945d9d356ef74d81d
Kojo Welbeck <kojo.welbeck99@gmail.com>
2013-04-09 02:50:04 -0400

    Adding the new ping activity

--- ProjectPing/gen/com/example/ping/R.java
+++ ProjectPing/gen/com/example/ping/R.java
@@ -14,19 +14,31 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int cancel_button=0x7f070007;
-        public static final int location_field=0x7f070003;
-        public static final int location_label=0x7f070002;
-        public static final int menu_settings=0x7f070008;
-        public static final int select_friends_button=0x7f070006;
-        public static final int time_field=0x7f070005;
-        public static final int time_label=0x7f070004;
-        public static final int title_field=0x7f070001;
-        public static final int title_label=0x7f070000;
+        public static final int button1=0x7f070001;
+        public static final int cancel_button=0x7f07000c;
+        public static final int friend1=0x7f07000e;
+        public static final int friend2=0x7f07000f;
+        public static final int friend3=0x7f070010;
+        public static final int friend4=0x7f070011;
+        public static final int friend5=0x7f070012;
+        public static final int friendLabel=0x7f07000d;
+        public static final int invited_friends_field=0x7f07000a;
+        public static final int invited_friends_label=0x7f070009;
+        public static final int location_field=0x7f070006;
+        public static final int location_label=0x7f070005;
+        public static final int menu_settings=0x7f070013;
+        public static final int new_ping=0x7f070002;
+        public static final int select_friends_button=0x7f07000b;
+        public static final int textView1=0x7f070000;
+        public static final int time_field=0x7f070008;
+        public static final int time_label=0x7f070007;
+        public static final int title_field=0x7f070004;
+        public static final int title_label=0x7f070003;
     }
     public static final class layout {
         public static final int activity_home=0x7f030000;
         public static final int activity_new_ping=0x7f030001;
+        public static final int fragment_sample_friends=0x7f030002;
     }
     public static final class menu {
         public static final int activity_home=0x7f060000;
@@ -34,12 +46,15 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int blank=0x7f04000c;
         public static final int cancel=0x7f040009;
         public static final int event_type=0x7f040004;
         public static final int hello_world=0x7f040001;
+        public static final int invited_friends=0x7f04000b;
         public static final int location=0x7f040007;
         public static final int menu_settings=0x7f040002;
         public static final int select_friends=0x7f040008;
+        public static final int selected_friends=0x7f04000a;
         public static final int time=0x7f040006;
         public static final int title=0x7f040005;
         public static final int title_activity_new_ping=0x7f040003;
--- ProjectPing/src/com/example/ping/NewPingActivity.java
+++ ProjectPing/src/com/example/ping/NewPingActivity.java
@@ -3,15 +3,57 @@ package com.example.ping;
 import com.example.ping.R;
 import android.os.Bundle;
 import android.app.Activity;
+
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 
-public class NewPingActivity extends Activity {
+public class NewPingActivity extends FragmentActivity {
+
+	protected static final String TAG = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_ping);
+		EditText newPingTitleField = (EditText) findViewById(R.id.title_field);
+		EditText newPingLocationField = (EditText) findViewById(R.id.location_field);
+		EditText newPingTimeField = (EditText) findViewById(R.id.time_field);
+		EditText newPingInvitedFriendsField = (EditText) findViewById(R.id.invited_friends_field);
+		Button selectFriendsButton = (Button) findViewById(R.id.select_friends_button);
+		Button cancelButton = (Button) findViewById(R.id.cancel_button);
+
+		selectFriendsButton.setOnClickListener(new OnClickListener(){
+
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				//if(getFragmentManager().findFragmentById(R.id.fragment) == null) {
+
+					Log.d(TAG,"adding fragment");
+
+
+
+					Fragment f = new SampleFriendsFragment();
+					//FragmentManager fragmentManager = getFragmentManager();
+
+					FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+					ft.add(R.id.new_ping, f);
+
+					ft.commit();
+
+				}
+
+			});
 	}
 
 	@Override
@@ -20,6 +62,6 @@ public class NewPingActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_new_ping, menu);
 		return true;
 	}
-	
+
 
 }
