9d50d277bf4cfb7505fd123bfb5aef0476efe511
Kojo Welbeck <kojo.welbeck99@gmail.com>
2013-05-06 20:40:03 -0400

    Adding logo navigation to Settings; layout change on activity_contact_view

--- ProjectPing/src/com/example/ping/SettingsActivity.java
+++ ProjectPing/src/com/example/ping/SettingsActivity.java
@@ -2,7 +2,9 @@ package com.example.ping;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class SettingsActivity extends Activity {
 
@@ -10,6 +12,7 @@ public class SettingsActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 
 	@Override
@@ -18,5 +21,19 @@ public class SettingsActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_settings, menu);
 		return true;
 	}
-
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// app icon in action bar clicked; go home
+			Intent intent = new Intent(this, EventsListActivity.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	
 }
